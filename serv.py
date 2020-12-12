ac97dde20730b1e4b5f2c19cc5c57c08ca6b7a3e
alxukr <alxnet@ukr.net>
2020-12-12 23:19:20 +0200

    multi server

--- serv.py
+++ serv.py
@@ -1,12 +1,18 @@
 import socket
+import threading
+
+def foo(co, ad):
+    while True:
+        data = co.recv(1024)
+        if data.decode() == "close": break
+        co.send(data)
+    co.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("0.0.0.0", 2222))
 s.listen(10)
 while True:
     conn, addr = s.accept()
-    while True:
-        data = conn.recv(1024)
-        if data.decode() == "close": break
-        conn.send(data)
-    conn.close()
+    np = threading.Thread(target=foo, args=(conn, addr))
+    np.start()
+
